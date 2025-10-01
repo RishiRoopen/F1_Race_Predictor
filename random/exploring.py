ed552f7b1d580ed3a5b619fd72a86dbb3fee2ac7
RishiRoopen <rishiroopen123@gmail.com>
2025-10-01 23:55:52 +0800

    change cache location

--- random/exploring.py
+++ random/exploring.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Enable the cache (so data is stored locally)
-fastf1.Cache.enable_cache(r"C:\UNI\Code\F1_Race_Predictor")  # replace 'cache' with your desired cache directory
+fastf1.Cache.enable_cache(r"C:\UNI\Code\F1_Race_Predictor\cache")  # replace 'cache' with your desired cache directory
 
 # Load Bahrain GP 2023 session data
 session = fastf1.get_session(2023, 'Bahrain', 'Q')
